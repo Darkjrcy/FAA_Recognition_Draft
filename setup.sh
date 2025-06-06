99691866db0273ed364f8fe6fd7abb2399a72442
Jorge Estupinan <jorgestu20@gmail.com>
2025-06-06 09:14:33 -0400

    Add the YOLO data zip file to the unzip

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "==== Downloading models.zip from Google Drive ===="
 
-# Dowload and unzip the MOdels folder
+# Dowload and unzip the Models folder
 # Google Drive file ID: 1612k0ajAOZc07ogzQaVJj58sSxcUIGob
-wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1612k0ajAOZc07ogzQaVJj58sSxcUIGob" -O models.zip
+MODELS_ID = "1612k0ajAOZc07ogzQaVJj58sSxcUIGob"
+gdown --id ${MODELS_ID} -O models.zip
 
 echo "=== Unzipping the files into the models folder ==="
 unzip -o models.zip -d src/plane_description/models/
@@ -11,10 +12,11 @@ unzip -o models.zip -d src/plane_description/models/
 
 # Dowload and unzip the data YOLO folder
 # Google Drive file ID: 1chHuow8cOx5b5a7lb4I7q1iILFkzfIup
-wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1chHuow8cOx5b5a7lb4I7q1iILFkzfIup" -O data.zip
+DATA_ID = "1chHuow8cOx5b5a7lb4I7q1iILFkzfIup"
+gdown --id ${DATA_ID} -O data.zip
 
 echo "=== Unzipping the files into the data folder ==="
-unzip -o models.zip -d src/airplane_recognition/
+unzip -o data.zip -d src/airplane_recognition/
 
 # Delete the zip files:
 echo "=== clean both zip files==="
